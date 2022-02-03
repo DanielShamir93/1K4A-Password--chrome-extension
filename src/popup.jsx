11a8fbfe869f19296b59e8670837100191a54821
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-02-03 19:19:40 +0200

    Create the environment

---
+++ src/popup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from "react-dom";
+
+function Popup() {
+  return (
+    <div>
+      wow
+    </div>
+  );
+}
+
+render(<Popup />, document.getElementById("react-target"));
