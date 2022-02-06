2629a6d034a005c9e8765242d71fe2bfdef23901
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-02-06 14:51:55 +0200

    :bug: Fix early comment of invalid email or password in Login component

--- src/components/login/Login.component.js
+++ src/components/login/Login.component.js
@@ -4,37 +4,49 @@ import Box from "@material-ui/core/Box";
 import BasicButton from "../basicButton/BasicButton.component";
 import PasswordInput from "../passwordInput/PasswordInput.components";
 import TextFieldInput from "../textFieldInput/TextFieldInput.component";
+import validator from "validator";
 import "./login.styles.scss";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [comment, setComment] = useState("");
-  
+
   const LOGIN_END_POINT = "users/login";
 
-  const login =  () => {
+  const login = () => {
     try {
-      myApi.post(LOGIN_END_POINT, { email, password })
-        .then((response) => {
-          const { data: userData } = response;
+      myApi.post(LOGIN_END_POINT, { email, password }).then((response) => {
+        const { data: userData } = response;
 
-          const loggedInUser = {
-            uid: userData.user._id,
-            email: userData.user.email,
-            token: userData.token
-          }
+        const loggedInUser = {
+          uid: userData.user._id,
+          email: userData.user.email,
+          token: userData.token,
+        };
 
-          chrome.storage.sync.set({ loggedInUser }, function() {
-            window.location.reload();
-          });
+        chrome.storage.sync.set({ loggedInUser }, function () {
+          window.location.reload();
         });
-      
+      });
     } catch (err) {
-      setComment(err.message);
       console.log(err.message);
     }
   };
+
+  const isValidInput = () => {
+    if (email.length === 0) {
+      setComment("Missing email")
+      return false;
+    } else if (password.length === 0) {
+      setComment("Missing password")
+      return false;
+    } else if(!validator.isEmail(email)) {
+      setComment("Invalid email")
+      return false;
+    }
+    return true;
+  }
 
   return (
     <div className="Login">
@@ -56,11 +68,22 @@ export default function Login() {
                 <TextFieldInput label="email" setEmail={setEmail} />
               </div>
               <div className="password">
-                <PasswordInput label="password" setPassword={setPassword}/>
+                <PasswordInput label="password" setPassword={setPassword} />
               </div>
             </Box>
           </div>
-          <BasicButton label="login" variant="contained" cb={login} />
+          <BasicButton
+            label="login"
+            variant="contained"
+            cb={() => {
+              if (isValidInput()){
+                login();
+                setTimeout(() => {
+                  setComment("Invalid email or password");
+                }, 2000)
+              }
+            }}
+          />
         </div>
         <div className="to-signup">
           {/* <a className="to-signup-text" type="_blank" href="https://one-key-for-all-paswords.herokuapp.com/">Signup</a> */}
